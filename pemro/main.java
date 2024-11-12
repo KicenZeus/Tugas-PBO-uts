c7375e7bdcedda25a6d20388eede03f1f85b561f
KicenZeus <tiandoputra12@gmail.com>
2024-11-13 01:13:55 +0700

    Update main.java

--- pemro/main.java
+++ pemro/main.java
@@ -14,7 +14,6 @@ public class Main {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
 
-        // Daftar penerima yang tersedia
         List<Penerima> daftarPenerima = new ArrayList<>();
         daftarPenerima.add(new Penerima("Zainab", "Fakir"));
         daftarPenerima.add(new Penerima("Ali", "Miskin"));
@@ -29,7 +28,6 @@ public class Main {
             System.out.print("Masukkan agama : ");
             String agama = input.nextLine();
 
-            // Cek jika agama bukan Islam
             if (!agama.equalsIgnoreCase("Islam")) {
                 System.out.println("Maaf, zakat hanya berlaku untuk umat Islam. Silakan coba lagi.\n");
                 continue; // Ulang input jika bukan Islam
@@ -39,14 +37,11 @@ public class Main {
             double jumlahHarta = input.nextDouble();
             input.nextLine(); // Membersihkan newline setelah input double
 
-            // Membuat objek Muzaki dengan data yang telah dimasukkan
             Muzaki muzaki = new Muzaki(nama, agama, jumlahHarta);
 
-            // Menampilkan informasi Muzaki
             System.out.println("\n=== Informasi Pembayaran Zakat ===");
             muzaki.displayInfo();
 
-            // Tampilkan penerima zakat hanya jika zakat lebih dari 0
             if (muzaki.getZakat() > 0) {
                 // Pilih penerima secara acak
                 Penerima penerimaAcak = daftarPenerima.get(random.nextInt(daftarPenerima.size()));
@@ -56,7 +51,6 @@ public class Main {
                 System.out.println("\nHarta kekayaan Anda tidak mencukupi nisab, tidak ada zakat yang harus dibayarkan.");
             }
 
-            // Menanyakan apakah ingin memasukkan data Muzaki lagi
             System.out.print("\nApakah Anda ingin memasukkan data Muzaki lagi? (ya/tidak): ");
             String jawab = input.nextLine();
             if (!jawab.equalsIgnoreCase("ya")) {
@@ -64,7 +58,6 @@ public class Main {
             }
         }
 
-        // Menutup scanner
         input.close();
     }
 }
